dda78118b569d30375bdf28b66a489c0e106e2a0
shakthepro <61986375+shakthepro@users.noreply.github.com>
2023-11-13 13:22:08 -0700

    not working

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 // background.js
 chrome.webNavigation.onCommitted.addListener((details) => {
-    chrome.tabs.executeScript(details.tabId, { file: "content.js" });
-  });
-  
-  chrome.tabs.onCreated.addListener((tab) => {
-    chrome.tabs.executeScript(tab.id, { file: "content.js" });
-  });
-  
+  console.log("onCommitted event triggered");
+  chrome.tabs.executeScript(details.tabId, { file: "content.js" });
+});
+
+chrome.tabs.onCreated.addListener((tab) => {
+  console.log("onCreated event triggered");
+  chrome.tabs.executeScript(tab.id, { file: "content.js" });
+});
--- content.js
+++ content.js
@@ -1,10 +1,15 @@
 // content.js
 const currentUrl = window.location.href;
 
-fetch("newfile.txt")
+  fetch("list.txt")
   .then((response) => response.text())
   .then((text) => {
-    if (text.includes(currentUrl)) {
-      document.body.parentNode.removeChild(document.body);
+    const blockedWebsites = text.split("\n").map((item) => item.trim());
+    // Check if the currentUrl matches any URL in the list
+    let isBlocked = false;
+    for (const url of blockedWebsites) {
+      if (currentUrl === url) {
+        document.body.parentNode.removeChild(document.body);
+      }
     }
   });
